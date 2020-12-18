86ee0a3aa9ae3c55bf15d5aa0df07294933a003f
José de la Torre López <jose@pc27-12.29009272.29.andared.ced.junta-andalucia.es>
2020-12-18 09:22:03 +0100

    Logout

--- js/welcome.js
+++ js/welcome.js
@@ -1,24 +1,30 @@
 $(document).ready(function(){
+	//Según se cargue el HTML, consume un servicio GET para obtener
+	//Todos los usuarios y los rellena en la tabla vacía results.
 	$.ajax({
         url: 'https://localhost:5001/usuario',
         dataType: 'json',
         type: 'get',
         contentType: 'application/json',
         success: function(data, status){
-            console.log("Data: " + data + "\nStatus: " + status);
-            $(function() {
-			    $.each(data, function(i, item) {
-			        var $tr = $('<tr>').append(
-			            $('<td>').text(item.username),
-			            $('<td>').text(item.name),
-			            $('<td>').text(item.apellidos),
-			            $('<td>').text(item.email),
-			            $('<td>').text(item.rol)
-			        ); //.appendTo('#records_table');
-			        console.log($tr.wrap('<p>').html());
-			        $('#results').append($tr);
-			    });
-			});
+            //Por cada elemento dentro del array data, construye una fila (tr)
+            //y añade celdas con los campos de cada elemento del array.
+		    $.each(data, function(i, item) {
+		        var $tr = $('<tr>').append(
+		            $('<td>').text(item.username),
+		            $('<td>').text(item.name),
+		            $('<td>').text(item.apellidos),
+		            $('<td>').text(item.email),
+		            $('<td>').text(item.rol)
+		        ); //.appendTo('#records_table');
+		        console.log($tr.wrap('<p>').html());
+		        $('#results').append($tr);
+		    });
+			
       	}
     });
 });
+
+function logout(){
+	window.location.replace("/Taller/views/login.html");
+}
